ef8914fbabfa28a8bc81bd827b226168b295288b
paleszgergo <paleszgergo@users.noreply.github.com>
2020-04-23 16:55:11 +0200

    update second part

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ name + "'s apples";
 console.log(name + "'s apples")
 console.log("Hello wolrd!")
 
+$('h1').text('We are haveing a break 10 min break until 17:05')
+
 // this is a javascript comment
 
